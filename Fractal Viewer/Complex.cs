49473b72889d27904aa14fc4975d32e3b9b3203d
Jakub Zimny <jakubzimny@outlook.com>
2018-05-26 23:34:13 +0200

    Complex class

--- Fractal Viewer/Complex.cs
+++ Fractal Viewer/Complex.cs
@@ -8,7 +8,31 @@ namespace Fractal_Viewer
 {
     class Complex
     {
-        private double a { get; set; } //real part
-        private double b { get; set; } // imaginary part
+        private double re { get; set; } //real part
+        private double im { get; set; } // imaginary part
+
+        public Complex(double r, double i)
+        {
+            re = r;
+            im = i;
+        }
+
+        public void Square()
+        {
+            double temp = re * re - im * im;
+            im = 2.0 * re * im;
+            re = temp;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(re*re + im*im);
+        }
+
+        public void Add(Complex number)
+        {
+            re += number.re;
+            im += number.im;
+        }
     }
 }
