9f5cbc33391c9bd8e4504cb21f2718ea9de4ce34
Jakub Zimny <jakubzimny@outlook.com>
2018-06-29 22:38:59 +0200

    Drag selection box added

--- Fractal Viewer/MainWindow.xaml.cs
+++ Fractal Viewer/MainWindow.xaml.cs
@@ -9,27 +9,30 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace Fractal_Viewer
 {
     public partial class MainWindow : Window
     {
-        WriteableBitmap bitmap;
-        PixelFormat pf;
-        int width, height, stride;
-        byte[] pixelData;
-        DispatcherTimer timer;
-        string chosenFractal;
-        string chosenColorScheme;
-        string renderTime;
-        double c_re;
-        double c_im;
-        List<int> sequence;
-        public const int iterMax = 1000;
+        private WriteableBitmap bitmap;
+        private PixelFormat pf;
+        private int width, height, stride;
+        private byte[] pixelData;
+        private DispatcherTimer timer;
+        private string chosenFractal;
+        private string chosenColorScheme;
+        private string renderTime;
+        private double c_re;
+        private double c_im;
+        private List<int> sequence;
+        public const int IterMax = 100;
+        bool mouseDown = false; // Set to 'true' when mouse is held down.
+        Point mouseDownPos; // The point where the mouse button was clicked down.
         public MainWindow()
         {
             InitializeComponent();
-            pf = PixelFormats.Rgb24; // 1 byte for each color
+            pf = PixelFormats.Rgb24; // Rgb24 = 1 byte for each color component
             width = (int)image.Width * 2; // *2 to increase resolution
             height = (int)image.Height * 2;
             stride = width * pf.BitsPerPixel / 8; // bytes per row
@@ -38,13 +41,15 @@ namespace Fractal_Viewer
             sequence = new List<int>();
         }
 
-        private void SetPixel(int x, int y, Color color, byte[] buffer, int stride)
+        private void SetPixel(int x, int y, Color color)
         {
+            // Evaluate position in pixelData array
             int xIndex = x * 3;
             int yIndex = y * stride;
-            buffer[xIndex + yIndex] = color.R;
-            buffer[xIndex + yIndex + 1] = color.G;
-            buffer[xIndex + yIndex + 2] = color.B;
+            // Set color value
+            pixelData[xIndex + yIndex] = color.R;
+            pixelData[xIndex + yIndex + 1] = color.G;
+            pixelData[xIndex + yIndex + 2] = color.B;
         }
 
         private void RenderMandelbrot()
@@ -60,14 +65,13 @@ namespace Fractal_Viewer
                     z = new Complex(0, 0);
                     c = new Complex(a, b);
                     int counter = 0;
-                    while (z.MagnitudeSquared() < 4 && counter++ < iterMax)
+                    while (z.MagnitudeSquared() < 4 && counter++ < IterMax)
                     {
                         z.Square();
                         z.Add(c);
                     }
-                    if (counter < iterMax) SetPixel(col, row, GetColorMapping(counter),
-                        pixelData, stride);
-                    else SetPixel(col, row, Colors.Black, pixelData, stride);
+                    if (counter < IterMax) SetPixel(col, row, GetColorMapping(counter));
+                    else SetPixel(col, row, Colors.Black);
                 }
             });
         }
@@ -85,15 +89,14 @@ namespace Fractal_Viewer
                     z = new Complex(0, 0);
                     c = new Complex(a, b);
                     int counter = 0;
-                    while (z.MagnitudeSquared() < 4 && counter++ < iterMax)
+                    while (z.MagnitudeSquared() < 4 && counter++ < IterMax)
                     {
                         z.Abs();
                         z.Square();
                         z.Add(c);
                     }
-                    if (counter < iterMax) SetPixel(col, row, GetColorMapping(counter),
-                        pixelData, stride);
-                    else SetPixel(col, row, Colors.Black, pixelData, stride);
+                    if (counter < IterMax) SetPixel(col, row, GetColorMapping(counter));
+                    else SetPixel(col, row, Colors.Black);
                 }
             });
         }
@@ -111,14 +114,13 @@ namespace Fractal_Viewer
                     z = new Complex(a, b);
                     c = new Complex(c_re, c_im);
                     int counter = 0;
-                    while (z.MagnitudeSquared() < 4 && counter++ < iterMax)
+                    while (z.MagnitudeSquared() < 4 && counter++ < IterMax)
                     {
                         z.Square();
                         z.Add(c);
                     }
-                    if (counter < iterMax) SetPixel(col, row, GetColorMapping(counter),
-                        pixelData, stride);
-                    else SetPixel(col, row, Colors.Black, pixelData, stride);
+                    if (counter < IterMax) SetPixel(col, row, GetColorMapping(counter));
+                    else SetPixel(col, row, Colors.Black);
                 }
 
             });
@@ -126,7 +128,6 @@ namespace Fractal_Viewer
 
         private void RenderLyapunovFractal()
         {
-            //int[] sequence = { 1, 0 };
             Parallel.For(0, height, row =>
             {
                 double a, b, r;
@@ -136,8 +137,9 @@ namespace Fractal_Viewer
                     double lambda = 0;
 
                     double x = 0.5;
-                    while (counter++ < 150)//capped at 150 for now
+                    while (counter++ < IterMax)
                     {
+                        // [2,4]x[2,4] region
                         a = (double)row / (double)height * 2 + 2;
                         b = (double)col / (double)width * 2 + 2;
                         //Lyapunov Exponent
@@ -146,7 +148,7 @@ namespace Fractal_Viewer
                         lambda += Math.Log(Math.Abs(r * (1.0 - 2.0 * x)));
                     }
                     lambda /= counter;
-                    SetPixel(col, row, GetColorLyapunov(lambda), pixelData, stride);
+                    SetPixel(col, row, GetColorLyapunov(lambda));
                 }
             });
         }
@@ -154,29 +156,37 @@ namespace Fractal_Viewer
         private Color GetColorLyapunov(double lambda)
         {
             //TODO: Change this method 
+
+            //Return Color value based on Lyapunov Exponent
+
             Color c;
-            lambda *= 100;
-            if (lambda < -280) c = Color.FromRgb(0, 0, 0);
-            else if (lambda < -260) c = Color.FromRgb(49, 25, 0);
-            else if (lambda < -230) c = Color.FromRgb(85, 50, 0);
-            else if (lambda < -200) c = Color.FromRgb(116, 70, 0);
-            else if (lambda < -180) c = Color.FromRgb(141, 90, 0);
-            else if (lambda < -150) c = Color.FromRgb(169, 112, 0);
-            else if (lambda < -120) c = Color.FromRgb(174, 119, 0);
-            else if (lambda < -100) c = Color.FromRgb(189, 127, 0);
-            else if (lambda < -80) c = Color.FromRgb(196, 138, 0);
-            else if (lambda < -50) c = Color.FromRgb(200, 149, 0);
-            else if (lambda < -20) c = Color.FromRgb(205, 162, 0);
-            else if (lambda < -10) c = Color.FromRgb(212, 169, 0);
-            else if (lambda < 0) c = Color.FromRgb(220, 175, 0);
-            else if (lambda > 40) c = Color.FromRgb(0, 0, 55);
-            else if (lambda > 35) c = Color.FromRgb(0, 0, 75);
-            else if (lambda > 30) c = Color.FromRgb(0, 0, 100);
-            else if (lambda > 25) c = Color.FromRgb(0, 0, 115);
-            else if (lambda > 20) c = Color.FromRgb(0, 0, 125);
-            else if (lambda > 15) c = Color.FromRgb(0, 0, 145);
-            else if (lambda > 10) c = Color.FromRgb(0, 0, 175);
-            else if (lambda > 5) c = Color.FromRgb(0, 0, 185);
+            lambda *= 100; // Just to make numbers clearer
+            if (lambda < -280) c = Color.FromRgb(35, 25, 0);
+            else if (lambda < -260) c = Color.FromRgb(59, 45, 0);
+            else if (lambda < -230) c = Color.FromRgb(85, 55, 0);
+            else if (lambda < -200) c = Color.FromRgb(116, 75, 0);
+            else if (lambda < -180) c = Color.FromRgb(141, 95, 0);
+            else if (lambda < -150) c = Color.FromRgb(165, 117, 0);
+            else if (lambda < -135) c = Color.FromRgb(168, 121, 0);
+            else if (lambda < -120) c = Color.FromRgb(171, 126, 0);
+            else if (lambda < -100) c = Color.FromRgb(177, 133, 0);
+            else if (lambda < -90) c = Color.FromRgb(179, 137, 0);
+            else if (lambda < -80) c = Color.FromRgb(182, 143, 0);
+            else if (lambda < -50) c = Color.FromRgb(185, 149, 0);
+            else if (lambda < -30) c = Color.FromRgb(190, 160, 0);
+            else if (lambda < -20) c = Color.FromRgb(195, 167, 0);
+            else if (lambda < -10) c = Color.FromRgb(200, 174, 0);
+            else if (lambda < -5) c = Color.FromRgb(205, 177, 0);
+            else if (lambda < 0) c = Color.FromRgb(210, 180, 0);
+            // lambda < 0 (yellow) means stability and lambda > 0 (blue) chaos
+            else if (lambda > 40) c = Color.FromRgb(0, 0, 185);
+            else if (lambda > 35) c = Color.FromRgb(0, 0, 175);
+            else if (lambda > 30) c = Color.FromRgb(0, 0, 145);
+            else if (lambda > 25) c = Color.FromRgb(0, 0, 125);
+            else if (lambda > 20) c = Color.FromRgb(0, 0, 115);
+            else if (lambda > 15) c = Color.FromRgb(0, 0, 100);
+            else if (lambda > 10) c = Color.FromRgb(0, 0, 75);
+            else if (lambda > 5) c = Color.FromRgb(0, 0, 55);
             else c = Color.FromRgb(0, 0, 200);
             return c;
         }
@@ -184,19 +194,23 @@ namespace Fractal_Viewer
         private void Render()
         {
             Stopwatch sw = new Stopwatch();
-            sw.Start();
+            sw.Start(); // Measure rendering time
+            // Call specific rendering method 
             if (chosenFractal == "Mandelbrot Set") RenderMandelbrot();
             else if (chosenFractal == "Burning Ship") RenderBurningShip();
             else if (chosenFractal == "Julia Set") RenderJuliaSet();
             else if (chosenFractal == "Lyapunov Fractal") RenderLyapunovFractal();
             sw.Stop();
+            // Set measured time
             renderTime = sw.Elapsed.TotalSeconds.ToString() + " s";
         }
 
         private void UpdateScreen(object o, System.EventArgs e)
         {
+            // Redraw whole image
             bitmap.WritePixels(new Int32Rect(0, 0, width, height), pixelData, stride, 0);
             image.Source = bitmap;
+            // Display rendering time
             if (renderTime != null)
                 timeTB.Text = renderTime;
             else timeTB.Text = "Rendering...";
@@ -204,11 +218,12 @@ namespace Fractal_Viewer
 
         private void renderButton_Click(object sender, RoutedEventArgs e)
         {
-            renderTime = null;
+            renderTime = null; // Null until value is measured
+            // Parse inputs
             chosenFractal = fractalCB.Text;
             chosenColorScheme = colorCB.Text;
             if (chosenFractal == "Julia Set")
-            { // parse only when Julia Set because it doesn't affect other fractals
+            { 
                 try
                 {
                     c_re = double.Parse(reTB.Text, System.Globalization.CultureInfo.InvariantCulture); // Invariant Culture in order to accept '.' 
@@ -225,38 +240,45 @@ namespace Fractal_Viewer
             {
                 parseSequence();
             }
-            Task t = Task.Factory.StartNew(Render); // Render in another thread so UI stays responsive
+            // Render in another thread so UI stays responsive
+            Task t = Task.Factory.StartNew(Render); 
+            // Every 50 miliseconds update screen
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(50);
-            timer.Tick += UpdateScreen; // every 50 miliseconds update displayed image
+            timer.Tick += UpdateScreen; 
             timer.Start();
         }
 
         private  void parseSequence()
         {
+            // Validate and parse sequence entered in sequence textbox
+
             string s = sequenceTB.Text;
-            sequence.Clear();
+            List<int> tempSequence = new List<int>();
             foreach (char c in s)
             {
-                if(!(c == '0' || c == '1'))
+                if(!(c == '0' || c == '1')) // '1' and '0' are only characters allowed
                 {
-                    System.Windows.MessageBox.Show("Entered sequence is not valid.\nUsing default sequence 10",
-                        "Parse Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                    sequence.Clear();
-                    sequence.Add(1);
-                    sequence.Add(0);
+                    System.Windows.MessageBox.Show("Entered sequence is not valid.\nUsing default sequence 1 0",
+                        "Parsing Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    tempSequence.Clear();
+                    tempSequence.Add(1);
+                    tempSequence.Add(0);
+                    sequence = tempSequence;
                     return;
                 }
                 else
                 {
-                    sequence.Add((int)c - (int)'0');
+                    tempSequence.Add((int)c - (int)'0');
                 }
             }
-            
+            sequence = tempSequence;
         }
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
+            // Convert bitmap to png and save at chosen path
+
             SaveFileDialog sfd = new SaveFileDialog()
             {
                 Filter = "Image Files(*.png)|*.png|All(*.*)|*"
@@ -274,6 +296,9 @@ namespace Fractal_Viewer
 
         private void fractalCB_DropDownClosed(object sender, EventArgs e)
         {
+            // Controlling which UI elements are enabled
+            // Disable ones that are not used by selected fractal
+
             chosenFractal = fractalCB.Text;
             if (colorCB != null && sequenceTB!=null)
             {
@@ -303,15 +328,89 @@ namespace Fractal_Viewer
             }
         }
 
+        private void windowGrid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            mouseDownPos = e.GetPosition(windowGrid);
+            // Check whether the point is inside image
+            if (mouseDownPos.X <= image.Width + image.Margin.Left
+                && mouseDownPos.X >= image.Margin.Left
+                && mouseDownPos.Y >= image.Margin.Top
+                && mouseDownPos.Y <= image.Height + image.Margin.Top)
+            {
+                mouseDown = true;
+            
+                windowGrid.CaptureMouse();
+
+                // Initial placement of the drag selection box        
+                Canvas.SetLeft(selectionBox, mouseDownPos.X);
+                Canvas.SetTop(selectionBox, mouseDownPos.Y);
+                selectionBox.Width = 0;
+                selectionBox.Height = 0;
+
+                // Make the drag selection box visible
+                selectionBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void windowGrid_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                // When the mouse is held down, reposition the drag 
+                // selection box but inside image only
+                Point mousePos = e.GetPosition(windowGrid);
+                if (mousePos.X <= image.Width + image.Margin.Left
+                && mousePos.X >= image.Margin.Left)
+                {
+                    if (mouseDownPos.X < mousePos.X)
+                    {
+                        Canvas.SetLeft(selectionBox, mouseDownPos.X);
+                        selectionBox.Width = mousePos.X - mouseDownPos.X;
+                    }
+                    else
+                    {
+                        Canvas.SetLeft(selectionBox, mousePos.X);
+                        selectionBox.Width = mouseDownPos.X - mousePos.X;
+                    }
+                }
+                if (mousePos.Y >= image.Margin.Top
+                && mousePos.Y <= image.Height + image.Margin.Top)
+                {
+                    if (mouseDownPos.Y < mousePos.Y)
+                    {
+                        Canvas.SetTop(selectionBox, mouseDownPos.Y);
+                        selectionBox.Height = mousePos.Y - mouseDownPos.Y;
+                    }
+                    else
+                    {
+                        Canvas.SetTop(selectionBox, mousePos.Y);
+                        selectionBox.Height = mouseDownPos.Y - mousePos.Y;
+                    }
+                }
+            }
+        }
+
+        private void windowGrid_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Release the mouse capture and stop tracking it
+            mouseDown = false;
+            windowGrid.ReleaseMouseCapture();
+
+            // Hide the drag selection box
+            selectionBox.Visibility = Visibility.Collapsed;
+        }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             timer.Stop();
         }
 
-        private Color GetColorMapping(int n)
+        private Color GetColorMapping(int iter)
         {
+            // Return Color based on number of iterations
+
             //TODO: Some gradients
-            int i = n % 16;
+            int i = iter % 16;
             Color[] mapping = new Color[16];
             switch (chosenColorScheme)
             {
